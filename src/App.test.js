69dca6f4d7e40ac143d47fbbad903600b5176163
Don Schrimsher <don4thirteen@gmail.com>
2019-08-08 17:47:44 -0400

    Add React testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import { render, cleanup } from "@testing-library/react"
+import App from "./App"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+afterEach(cleanup)
+
+it("renders without crashing", () => {
+  const { container } = render(<App />)
+  expect(container).not.toBeNull()
+})
