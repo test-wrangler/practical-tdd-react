078ea9166d52ebb6f93d3c41817cdc7d585aec2f
Don Schrimsher <don4thirteen@gmail.com>
2019-08-12 12:52:48 -0400

    Solution 6: Custom Commands

--- cypress/integration/taco.spec.js
+++ cypress/integration/taco.spec.js
@@ -1,14 +1,7 @@
 describe("Ordering tacos", () => {
   it("allows the user to order tacos", () => {
     cy.visit("http://localhost:3000")
-    getByDataCy("gimmeTacos").click()
-    getByDataCy("tacoName", { text: "Steak and Cilantro" })
+    cy.get({ dataCy: "gimmeTacos" }).click()
+    cy.get({ dataCy: "tacoName", text: "Steak and Cilantro" })
   })
 })
-
-function getByDataCy(name, options = {}) {
-  const { text } = options
-  let selectorString = `[data-cy="${name}"]`
-  if (text) selectorString += `:contains("${text}")`
-  return cy.get(selectorString)
-}
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,48 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.overwrite("get", (...args) => {
+  const [originalFn, options] = args
+  if (typeof options === "object") {
+    maybeThrowDataCyError(options)
+    const selector = buildSelector(options)
+    const log = buildDataCyLog(selector)
+    return originalFn(selector, { log: false }).then($elements => {
+      setConsoleProps({ options, $elements, selector, log })
+      return $elements
+    })
+  }
+
+  return originalFn(...Cypress._.tail(args))
+})
+
+const maybeThrowDataCyError = options => {
+  if (!options.dataCy)
+    throw new Error(
+      "Calling `get` with an object isn't supported without a `dataCy` property"
+    )
+}
+
+const buildDataCyLog = selector =>
+  Cypress.log({
+    name: "(Custom) GET",
+    displayName: "GET",
+    message: `(Custom) ${selector}`
+  })
+
+const setConsoleProps = ({ options, $elements, selector, log }) =>
+  log.set({
+    consoleProps: () => ({
+      ...options,
+      yielded: $elements,
+      elements: $elements.length,
+      selector
+    })
+  })
+
+const buildSelector = options => {
+  const selector = `[data-cy="${options.dataCy}"]`
+  if (!options.text) return selector
+  return selector + `:contains("${options.text}")`
+}
